ab8cecf3cc9874f871b21d5fd9eec2814dab099c
Ryan-Kendrick <118949957+Ryan-Kendrick@users.noreply.github.com>
2023-03-17 11:28:38 +1300

    add div for form inputs

--- client/components/Form.tsx
+++ client/components/Form.tsx
@@ -76,70 +76,72 @@ function Form() {
       <h2>Very Important Form</h2>
       <div className="form">
         <form onSubmit={submitHandler}>
-          <div className="name">
-            <div className="firstName">
-              <label htmlFor="firstName">First Name:</label>
+          <div className="formdata">
+            <div className="name">
+              <div className="firstName">
+                <label htmlFor="firstName">First Name:</label>
+                <input
+                  id="firstName"
+                  name="firstName"
+                  value={formData.firstName}
+                  onChange={changeHandler}
+                />
+              </div>
+              <div className="lastName">
+                <label htmlFor="lastName">Last Name:</label>
+                <input
+                  id="lastName"
+                  name="lastName"
+                  value={formData.lastName}
+                  onChange={changeHandler}
+                />
+              </div>
+            </div>
+            <div className="company">
+              <label htmlFor="company">Company:</label>
               <input
-                id="firstName"
-                name="firstName"
-                value={formData.firstName}
+                id="company"
+                name="company"
+                value={formData.company}
                 onChange={changeHandler}
               />
             </div>
-            <div className="lastName">
-              <label htmlFor="lastName">Last Name:</label>
+            <div className="email">
+              <label htmlFor="email">Email address:</label>
               <input
-                id="lastName"
-                name="lastName"
-                value={formData.lastName}
+                id="email"
+                name="email"
+                value={formData.email}
                 onChange={changeHandler}
               />
             </div>
-          </div>
-          <div className="company">
-            <label htmlFor="company">Company:</label>
-            <input
-              id="company"
-              name="company"
-              value={formData.company}
-              onChange={changeHandler}
-            />
-          </div>
-          <div className="email">
-            <label htmlFor="email">Email address:</label>
-            <input
-              id="email"
-              name="email"
-              value={formData.email}
-              onChange={changeHandler}
-            />
-          </div>
-          <div className="phone">
-            <label htmlFor="phone">Phone Number:</label>
-            <input
-              id="phone"
-              name="phone"
-              value={formData.phone}
-              onChange={changeHandler}
-              type="number"
-            />
-          </div>
-          <div className="password">
-            <label htmlFor="password">Password:</label>
-            <input
-              id="password"
-              name="password"
-              value={formData.password}
-              onChange={changeHandler}
-            />
-            <div className="reenterPassword">
-              <label htmlFor="reenterPassword">Reenter password:</label>
+            <div className="phone">
+              <label htmlFor="phone">Phone Number:</label>
               <input
-                id="reenterPassword"
-                name="reenterPassword"
-                value={formData.reenterPassword}
+                id="phone"
+                name="phone"
+                value={formData.phone}
+                onChange={changeHandler}
+                type="number"
+              />
+            </div>
+            <div className="password">
+              <label htmlFor="password">Password:</label>
+              <input
+                id="password"
+                name="password"
+                value={formData.password}
                 onChange={changeHandler}
               />
+              <div className="reenterPassword">
+                <label htmlFor="reenterPassword">Reenter password:</label>
+                <input
+                  id="reenterPassword"
+                  name="reenterPassword"
+                  value={formData.reenterPassword}
+                  onChange={changeHandler}
+                />
+              </div>
             </div>
           </div>
           <div className="form-buttons">

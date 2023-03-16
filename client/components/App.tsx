a84c039770a3baa8792b1e939bf52bd77f14039d
student <student@pop-os.localdomain>
2023-03-17 09:52:16 +1300

    add routes

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,29 +1,53 @@
+import { Routes, Route } from 'react-router-dom'
+import { useState } from 'react'
+
+// import Splash from './Splash'
+import ToS from './ToS'
+import Form from './Form'
+
 function App() {
   return (
     <>
       <header>
         <nav>
           <ul>
-            <li><a href="/">Home</a></li>
-            <li><a href="#about">About</a></li>
-            <li><a href="#contact">Contact</a></li>
+            <li>
+              <a href="/">Home</a>
+            </li>
+            <li>
+              <a href="#about">About</a>
+            </li>
+            <li>
+              <a href="#contact">Contact</a>
+            </li>
           </ul>
           <div className="navButtons">
             <button className="navButton">LOG IN</button>
             <button className="navButton">SIGN UP</button>
           </div>
         </nav>
-        <a className="logo" href="/"><img src="/images/logo.svg" alt="" /></a>
+        <a className="logo" href="/">
+          <img src="/images/logo.svg" alt="" />
+        </a>
       </header>
       <div className="info">
         <div className="words">
           <h1>Very Important Service:</h1>
-          <p>React Lorem ipsum dolor sit amet consectetur adipisicing elit. In hic excepturi tempora fuga molestiae sed veritatis consectetur animi corporis voluptas explicabo quae odit, aspernatur itaque natus doloremque omnis cum praesentium! has begun!</p>
+          <p>
+            React Lorem ipsum dolor sit amet consectetur adipisicing elit. In
+            hic excepturi tempora fuga molestiae sed veritatis consectetur animi
+            corporis voluptas explicabo quae odit, aspernatur itaque natus
+            doloremque omnis cum praesentium! has begun!
+          </p>
           <button>SIGN UP NOW!</button>
-
         </div>
-        <img className="manImage"src="/images/illustration.png" alt="" />
+        <img className="manImage" src="/images/illustration.png" alt="" />
       </div>
+      <Routes>
+        {/* <Route path="/" element={<Splash />}></Route> */}
+        <Route path="/tos" element={<ToS />}></Route>
+        <Route path="/form" element={<Form />}></Route>
+      </Routes>
     </>
   )
 }

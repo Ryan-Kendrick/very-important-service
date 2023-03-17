3ace3ab834e2d331cfeb09336a3d306dfd9a92d6
Louis Dyer <111328948+doilyuser@users.noreply.github.com>
2023-03-17 13:34:31 +1300

    Make ToS more annoying

--- client/components/ToS.tsx
+++ client/components/ToS.tsx
@@ -57,6 +57,57 @@ function ToS() {
             you may request a review, the approximate wait time is currently 99
             years.
           </li>
+          <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+          <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+          <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+          <li>hi</li><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+          <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+          <li>still here?</li><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+          <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+          <li>dedicated.</li><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+          <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+          <li>just keep scrolling.</li><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+          <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+          <li>nearly there.</li><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+          <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+          <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+          <li>
+            We reserve the right to collect your data, then sell it to third
+            parties. Since we&apos;re allowing you to use our site for free,
+            it's only fair, right?
+          </li>
+          <li>
+            Your collected data will also be used for targeted advertisements.
+            Remember that thing you looked at on Amazon last week? No? Well
+            here&apos;s a reminder on a sidebar ad.
+          </li>
+          <li>
+            Though we don&apos;t have unauthorized access to your microphone or
+            camera, you will somehow get advertisements for things that you have
+            only mentioned in person, not on your phone or computer, we swear
+            it's just a coincidence though…
+          </li>
+          <li>
+            Upload, any pictures to our site? Thanks, they&apos;re ours now, we
+            can legally sell them and use them for advertisements, we probably
+            won&apos;t, but we can.
+          </li>
+          <li>
+            Haven&apos;t made an account on our app yet? Don&apos;t worry, we
+            will use the information we&apos;ve collected about you and make one
+            for you.
+          </li>
+          <li>
+            Though we spend more money than you'll ever make in your life on
+            cybersecurity, your information will probably be stolen in a data
+            breach, just accept it and move on.
+          </li>
+          <li>
+            We reserve the right to delete your account for no reason, despite
+            allowing neo-nazis to post hate speech without repercussion. While
+            you may request a review, the approximate wait time is currently 99
+            years.
+          </li>
         </ol>
       </div>
       <div className="form-buttons">
